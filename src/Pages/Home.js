089435b6fc863e9ee2b095c830525f049a3c7938
AccidentalCodes <siyayoudeen@gmail.com>
2023-05-17 20:04:20 +0530

    new

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,61 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../Img/aglogo.png";
 import Footer from "../Components/Footer";
+import { Modal } from "antd";
 
 const Home = () => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <div className="bg-black">
-      <div className="px-16 py-10 items-center  flex flex-row justify-between ">
-        <img src={Logo} className="w-60 " alt="Logo" />
-        <div className="flex flex-row ">
-          <div className="text-white text-2xl m-5 font-['Noto Sans SC']">
-            credit score check
+    <>
+      <div className="bg-black">
+        {/* <div className="px-16 py-10 items-center  flex flex-row justify-between ">
+          <img src={Logo} className="w-60 " alt="Logo" />
+          <div className="flex flex-row ">
+            <div className="text-white text-2xl m-5 font-['Noto Sans SC']">
+              credit score check
+            </div>
+            <div className="text-white text-2xl m-5 font-['Noto Sans SC']">
+              {" "}
+              CRED pay
+            </div>
           </div>
-          <div className="text-white text-2xl m-5 font-['Noto Sans SC']">
-            {" "}
-            CRED pay
+        </div> */}
+
+        <div className="text-white p-5 flex flex-col  bg-no-repeat  items-center justify-center ">
+          <div className="text-7xl sm:text-9xl font-['Noto Sans SC'] font-extrabold pt-40 ">
+            rewards for paying
+            <div className="text-7xl sm:text-9xl font-['Noto Sans SC']">
+              credit card bills.
+            </div>
+          </div>
+          <div className="text-3xl font-['Noto Sans SC'] mt-10">
+            join 9M+ members who win rewards and cashbacks everyday
+          </div>
+          <div className="mt-16 mb-96">
+            <button class="bg-gray-700 hover:bg-blue-700 text-white font-bold py-5 px-20  rounded-full">
+              Learn More
+            </button>
           </div>
         </div>
-      </div>
-      <div className="text-white mt-16 m-5 flex flex-col  bg-no-repeat  items-center justify-center ">
-        <div className="text-9xl font-['Noto Sans SC'] font-extrabold ">
-          rewards for paying
-          <div className="text-9xl font-['Noto Sans SC']">
-            credit card bills.
+
+        <div className="text-white p-10 min-w-screen min-h-screen bg-no-repeat bg-right object-contain bg-[url('/Volumes/SIYA/Git/ag/src/Img/bgm7.jpeg')] ">
+          <div className="text-7xl sm:text-9xl  font-['Noto Sans SC'] font-semibold pt-20  ">
+            Be Secure
+            <div>Feel Safe</div>
+          </div>
+          <div className="text-3xl font-['Noto Sans SC'] mt-10">
+            Be an organisation that is secured with an Intenational
+            standard....!
+          </div>
+          <div className="text-1xl md:text-2xl font-['Noto Sans SC'] mt-20  w-full md:pr-96">
+            Secure your business with our expert Information Security Audit
+            services! Our team of CISAs specializes in conducting audits for
+            various compliance standards including ISO 27001, GDPR, SOC 2, and
+            more. With years of experience, our audits will ensure that your
+            company's sensitive information is protected. Trust us to keep your
+            business compliant and secure. Contact us now to schedule an audit!
+          </div>
+          <div className=" mt-10">
+            <button class="bg-black hover:bg-blue-700 text-white font-bold py-5 px-20  rounded-full">
+              Learn More
+            </button>
           </div>
         </div>
-        <div className="text-3xl font-['Noto Sans SC'] mt-10">
-          join 9M+ members who win rewards and cashbacks everyday
-        </div>
-        <div className="mt-16 mb-96">
-          <button class="bg-gray-700 hover:bg-blue-700 text-white font-bold py-5 px-20  rounded-full">
-            Learn More
-          </button>
-        </div>
-      </div>
-      <div className="text-white p-10 min-w-screen min-h-screen bg-no-repeat bg-right object-contain bg-[url('/Volumes/SIYA/Git/ag/src/Img/bgm7.jpeg')] ">
-        <div className="text-9xl font-['Noto Sans SC'] font-semibold pt-40  ">
-          Be Secure
-          <div>Feel Safe</div>
-        </div>
-        <div className="text-3xl font-['Noto Sans SC'] mt-10">
-          Be an organisation that is secured with an Intenational standard....!
-        </div>
-        <div className="text-2xl font-['Noto Sans SC'] mt-20  w-full pr-96">
-          Secure your business with our expert Information Security Audit
-          services! Our team of CISAs specializes in conducting audits for
-          various compliance standards including ISO 27001, GDPR, SOC 2, and
-          more. With years of experience, our audits will ensure that your
-          company's sensitive information is protected. Trust us to keep your
-          business compliant and secure. Contact us now to schedule an audit!
-        </div>
-        <div className=" mt-10">
-          <button class="bg-black hover:bg-blue-700 text-white font-bold py-5 px-20  rounded-full">
-            Learn More
-          </button>
-        </div>
-      </div>
-      <div className="text-white p-10  bg-no-repeat bg-right bg-contain min-w-screen min-h-screen bg-[url('/Volumes/SIYA/Git/ag/src/Img/bgm5.jpeg')] ">
+
+        {/* <div className="text-white p-10  bg-no-repeat bg-right bg-contain min-w-screen min-h-screen bg-[url('/Volumes/SIYA/Git/ag/src/Img/bgm5.jpeg')] ">
         <div className="text-9xl font-['Noto Sans SC'] font-semibold pt-40  ">
           Thinking of Vulnerability
           <div>testing</div>
@@ -63,7 +71,7 @@ const Home = () => {
         <div className="text-3xl font-['Noto Sans SC'] mt-10">
           With the team of Certified Ethical hackers with immensive experiences
         </div>
-        <div className="text-2xl font-['Noto Sans SC'] mt-20  w-full pr-96">
+        <div className="text-2xl font-['Noto Sans SC'] mt-20  w-full md:pr-96">
           Protect your business from cyber threats with our VAPT testing
           services! Our expert team will identify vulnerabilities in your
           systems and provide actionable recommendations to mitigate them. Stay
@@ -71,12 +79,33 @@ const Home = () => {
           now to schedule a testing!
         </div>
         <div className=" mt-10">
-          <button class="bg-black hover:bg-blue-700 text-white font-bold py-5 px-20  rounded-full">
+          <button
+            class="bg-black hover:bg-blue-700 text-white font-bold py-5 px-20  rounded-full"
+            onClick={() => setOpen(true)}
+          >
             Learn More
           </button>
+          <Modal
+            title="Certified Ethical hackers"
+            centered
+            open={open}
+            onOk={() => setOpen(false)}
+            okType="primary"
+            onCancel={() => setOpen(false)}
+          >
+            <p>
+              {" "}
+              Protect your business from cyber threats with our VAPT testing
+              services! Our expert team will identify vulnerabilities in your
+              systems and provide actionable recommendations to mitigate them.
+              Stay secure and stay ahead of the game with our VAPT services.
+              Contact us now to schedule a testing!
+            </p>
+          </Modal>
         </div>
-      </div>
-      <div className="text-white bg-[#0e1339]  bg-no-repeat bg-left p-10 min-w-screen min-h-screen bg-[url('/Volumes/SIYA/Git/ag/src/Img/bgm1.jpeg')] ">
+      </div> */}
+
+        {/* <div className="text-white bg-[#0e1339]  bg-no-repeat bg-left p-10 min-w-screen min-h-screen bg-[url('/Volumes/SIYA/Git/ag/src/Img/bgm1.jpeg')] ">
         <div className="grid justify-items-end ">
           <div className="text-9xl text-right font-['Noto Sans SC']  font-semibold pt-40  ">
             Wanna be an expert in Information security
@@ -97,11 +126,13 @@ const Home = () => {
             </button>
           </div>
         </div>
+      </div> */}
+
+        {/* <div>
+          <Footer />
+        </div> */}
       </div>
-      <div>
-        <Footer />
-      </div>
-    </div>
+    </>
   );
 };
 
