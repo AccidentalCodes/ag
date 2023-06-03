2a69aaca650aa201f7ad5de23f751cb5b6e1da81
AccidentalCodes <siyayoudeen@gmail.com>
2023-06-03 12:50:32 +0530

    new

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -32,7 +32,6 @@ const Home = () => {
           </div>
         </nav>
 
-        
         {/* <div className="bg-black p-10 items-center  flex flex-row justify-between ">
           <img src={Logo} className="w-60" alt="Logo" />
           <div className="flex flex-row  ">
@@ -231,7 +230,7 @@ const Home = () => {
           />
         </div> */}
 
-        <div className="text-white px-10 py-40  lg:px-20  lg:py-60 bg-no-repeat bg-right  object-cover min-w-screen h-full bg-cover bg-[url('/Volumes/SIYA/Git/ag/src/Img/bg5.jpg')] ">
+        <div className="text-white px-10 py-40  lg:px-20  lg:py-60 bg-no-repeat   object-cover min-w-screen h-full bg-cover  bg-[url('/Volumes/SIYA/Git/ag/src/Img/bg5.jpg')] ">
           <div
             className="text-6xl  md:text-7xl xl:text-9xl font-['Poppins'] font-bold  "
             data-aos="fade-right"
@@ -319,37 +318,119 @@ const Home = () => {
           </div>
         </div>
 
-        <div className="text-white p-10 bg-no-repeat bg-right  object-cover min-w-screen h-full bg-cover bg-[url('/Volumes/SIYA/Git/ag/src/Img/bgm5.jpeg')] ">
+        <div className="text-white px-10 py-40  lg:px-20  lg:py-60   bg-no-repeat bg-cover bg-right  object-cover min-w-screen min-h-screen bg-[url('/Volumes/SIYA/Git/ag/src/Img/bgm5.jpeg')] ">
           <div
-            className="text-5xl lg:text-6xl xl:text-8xl font-['Noto Sans SC'] font-semibold pt-40 md:pt-60 lg:pt-40 "
-            data-aos="fade-right"
+            className="text-6xl  md:text-7xl xl:text-9xl font-['Noto Sans SC'] font-semibold text-right pt-20 "
+            data-aos="fade-left"
             data-aos-duration="1000"
           >
             Thinking of Vulnerability
             <div>testing</div>
           </div>
           <div
-            className="text-3xl font-['Noto Sans SC'] mt-10"
+            className="text-3xl font-['Noto Sans SC'] text-right mt-10"
             data-aos="fade-up"
             data-aos-duration="1000"
           >
             With the team of Certified Ethical hackers with immensive
             experiences
           </div>
-          <div
-            className="text-1xl md:text-2xl  font-['Noto Sans SC'] mt-20  w-full "
-            data-aos="fade-right"
-            data-aos-duration="1000"
-          >
+          <div className="text-1xl md:text-2xl  font-['Poppins']   lg:text-md mt-10 text-right  w-full ">
             Protect your business from cyber threats with our VAPT testing
             services! Our expert team will identify vulnerabilities in your
             systems and provide actionable recommendations to mitigate them.
             Stay secure and stay ahead of the game with our VAPT services.
             Contact us now to schedule a testing!
           </div>
-          <div className=" mt-10">
+          <div className="grid justify-items-end ">
+            <div className=" mt-10"></div>
             <button
               className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+              type="button"
+              onClick={() => setShowModal(true)}
+            >
+              Open regular modal
+            </button>
+            {showModal ? (
+              <>
+                <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+                  <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                    {/*content*/}
+                    <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                      {/*header*/}
+                      <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                        <h3 className="text-3xl font-semibold">Modal Title</h3>
+                        <button
+                          className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                          onClick={() => setShowModal(false)}
+                        >
+                          <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                            ×
+                          </span>
+                        </button>
+                      </div>
+                      {/*body*/}
+                      <div className="relative p-6 flex-auto">
+                        <p className="my-4 text-slate-500 text-lg leading-relaxed">
+                          I always felt like I could do anything. That’s the
+                          main thing people are controlled by! Thoughts- their
+                          perception of themselves! They're slowed down by their
+                          perception of themselves. If you're taught you can’t
+                          do anything, you won’t do anything. I was taught I
+                          could do everything.
+                        </p>
+                      </div>
+                      {/*footer*/}
+                      <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                        <button
+                          className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                          type="button"
+                          onClick={() => setShowModal(false)}
+                        >
+                          Close
+                        </button>
+                        <button
+                          className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                          type="button"
+                          onClick={() => setShowModal(false)}
+                        >
+                          Save Changes
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+              </>
+            ) : null}
+          </div>
+        </div>
+
+        <div className="text-white bg-[#0e1339]  px-10 py-40  lg:px-20  lg:py-60 bg-no-repeat   object-obtain min-w-screen h-full bg-cover bg-[url('/Volumes/SIYA/Git/ag/src/Img/bg5.jpg')] ">
+          <div
+            className="text-6xl  md:text-7xl xl:text-9xl font-['Poppins'] font-bold  "
+            data-aos="fade-right"
+            data-aos-duration="1000"
+          >
+            Wanna be an expert in Information security
+          </div>
+          <div
+            className="text-3xl font-['Poppins'] font-semibold mt-10"
+            data-aos="fade-up"
+            data-aos-duration="1000"
+          >
+            Or to equip you're employees in Infosec
+          </div>
+          <div className="text-1xl md:text-2xl  font-['Poppins'] lg:w-3/5 lg:text-md mt-10  w-full ">
+            Advance your career in information security with our comprehensive
+            training programs! Our experts offer a range of courses, including
+            Information Security and CEH Training, designed to provide practical
+            knowledge and skills to protect sensitive information. Enroll now
+            and become a certified information security professional!
+          </div>
+          <div className=" mt-10">
+            <button
+              className="bg-pink-500 text-white  active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
               type="button"
               onClick={() => setShowModal(true)}
             >
@@ -441,92 +522,6 @@ const Home = () => {
             </div>
           </div>
         </div> */}
-
-        <div className="text-white p-10 bg-[#0e1339]  bg-no-repeat bg-right  object-cover min-w-screen min-h-screen bg-[url('/Volumes/SIYA/Git/ag/src/Img/bgm1.jpeg')] ">
-          <div
-            className="text-5xl lg:text-6xl xl:text-8xl font-['Noto Sans SC'] font-semibold text-right pt-40 "
-            data-aos="fade-left"
-            data-aos-duration="1000"
-          >
-            Wanna be an expert in Information security
-          </div>
-          <div
-            className="text-3xl font-['Noto Sans SC'] text-right mt-10"
-            data-aos="fade-up"
-            data-aos-duration="1000"
-          >
-            Or to equip you're employees in Infosec
-          </div>
-          <div className="text-1xl md:text-2xl text-right font-['Noto Sans SC'] mt-20  w-full ">
-            Advance your career in information security with our comprehensive
-            training programs! Our experts offer a range of courses, including
-            Information Security and CEH Training, designed to provide practical
-            knowledge and skills to protect sensitive information. Enroll now
-            and become a certified information security professional!
-          </div>
-          <div className="grid justify-items-end ">
-            <div className=" mt-10"></div>
-            <button
-              className="bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-              type="button"
-              onClick={() => setShowModal(true)}
-            >
-              Open regular modal
-            </button>
-            {showModal ? (
-              <>
-                <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-                  <div className="relative w-auto my-6 mx-auto max-w-3xl">
-                    {/*content*/}
-                    <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                      {/*header*/}
-                      <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                        <h3 className="text-3xl font-semibold">Modal Title</h3>
-                        <button
-                          className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                          onClick={() => setShowModal(false)}
-                        >
-                          <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                            ×
-                          </span>
-                        </button>
-                      </div>
-                      {/*body*/}
-                      <div className="relative p-6 flex-auto">
-                        <p className="my-4 text-slate-500 text-lg leading-relaxed">
-                          I always felt like I could do anything. That’s the
-                          main thing people are controlled by! Thoughts- their
-                          perception of themselves! They're slowed down by their
-                          perception of themselves. If you're taught you can’t
-                          do anything, you won’t do anything. I was taught I
-                          could do everything.
-                        </p>
-                      </div>
-                      {/*footer*/}
-                      <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
-                        <button
-                          className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                          type="button"
-                          onClick={() => setShowModal(false)}
-                        >
-                          Close
-                        </button>
-                        <button
-                          className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                          type="button"
-                          onClick={() => setShowModal(false)}
-                        >
-                          Save Changes
-                        </button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
-              </>
-            ) : null}
-          </div>
-        </div>
 
         {/* <div className="text-white bg-[#0e1339]  bg-no-repeat bg-left p-10 min-w-screen min-h-screen bg-[url('/Volumes/SIYA/Git/ag/src/Img/bgm1.jpeg')] ">
           <div className="grid justify-items-end ">
