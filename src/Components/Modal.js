772ccb2e92d9574d0cf2431a78a71e0669a9e6ca
AccidentalCodes <siyayoudeen@gmail.com>
2023-06-14 21:17:15 +0530

    new

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -3,15 +3,28 @@ import React from "react";
 const Modal = ({ visible, onClose }) => {
   if (!visible) return null;
   return (
-    <div
-      className="fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex 
-     items-center justify-center"
-    >
+    <div className="fixed inset-0 bg-white ">
       <div
-        className="bg-white p-8 h-full rounded flex flex-col 
+        className="flex 
+     items-center justify-end text-4xl mt-10 mr-10"
+      >
+        <div
+          onClick={onClose}
+          className="text-black cursor-pointer hover:text-red-500"
+        >
+          x
+        </div>
+      </div>
+
+      <div
+        className="p-8 h-full rounded flex flex-col 
      items-center justify-center "
       >
-        <div className="text-slate-500 text-1xl md:text-3xl p-4 font-['Poppins'] mb-10 ">
+        <div
+          className="text-slate-500 text-1xl md:text-3xl p-4 font-['Poppins']  "
+          data-aos="fade-right"
+          data-aos-duration="1000"
+        >
           I always felt like I could do anything. That’s the main thing people
           are controlled by! Thoughts- their perception of themselves! They're
           slowed down by their perception of themselves. If you're taught you
@@ -19,14 +32,14 @@ const Modal = ({ visible, onClose }) => {
           everything.
         </div>
 
-        <div>
+        {/* <div>
           <button
             onClick={onClose}
             class="bg-gray-700 hover:bg-blue-700  text-white font-bold py-5 px-20 rounded"
           >
             close
           </button>
-        </div>
+        </div> */}
       </div>
     </div>
   );
